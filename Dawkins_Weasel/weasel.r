d20544c6755e069fb2c9855e858a550a2136dd2c
Maciej <maciejmiliszewski@gmail.com>
2019-04-01 23:49:52 +0200

    major improvements in performance

--- Dawkins_Weasel/weasel.r
+++ Dawkins_Weasel/weasel.r
@@ -1,46 +1,25 @@
-# str <- "METHINKS IT IS LIKE A WEASEL"
-
 library(stringi)
-str1 <- stri_rand_strings(1, 28, pattern = "[A-Z]")
-str2 <- stri_rand_strings(1, 28, pattern = "[A-Z]")
-str3 <- stri_rand_strings(1, 28, pattern = "[A-Z]")
-str4 <- stri_rand_strings(1, 28, pattern = "[A-Z]")
-vect <- stri_rand_strings(100, 28, pattern = "[A-Z]")
-# print(str1, str2)
 
 score <- function(a, b){
-	empt <- c()
-	second <- unlist(strsplit(b, ''))
-	for (i in a){
-		first <- unlist(strsplit(i, ''))
-		logic_vector <- first == second
-		empt <- append(empt, sum(logic_vector))
-		# print(first)
-		# print(second)
-	}	
-	return(empt/28)
+	mx <- sapply(a, function(x) sum(unlist(strsplit(x, '')) == unlist(strsplit(b, ''))))
+	return(unname(mx/nchar(a)))
 }
 
 mutate <- function(a){
-	empt <- c()
-	lettersspc <- append(LETTERS, ' ')
-	for (a_part in a){
-		a_part <- unlist(strsplit(a_part, ''))
-		for (j in seq(a_part)){
-			prob <- runif(1)
-			if (prob < 0.05){
-				a_part[j] <- sample(lettersspc, 1)
-			}
-		}
-		empt <- append(empt, paste((a_part), collapse=''))
-	}
-	return(empt)
+	letters_spc <- c(LETTERS, ' ')
+	a <- sapply(a, function(x){
+		strr <- unlist(strsplit(x, ''))
+		prob <- runif(nchar(x))
+		strr[prob <= 0.05] <- sample(letters_spc, length(which(prob <= 0.05)), replace = TRUE)
+		paste(strr, collapse = '')
+		})
+	return(unname(a))
 }
 
-generation <- function(curBest, mutator, scorer, population){
+generation <- function(curBest, mutator, scorer, population, tgt){
 	v <- rep(curBest, population)
 	v_mut <- mutate(v)
-	v_score <- score(v_mut, "METHINKS IT IS LIKE A WEASEL")
+	v_score <- score(v_mut, tgt)
 	newBest <- v_mut[which(v_score == max(v_score))[1]]
 	scores <- sort(v_score, decreasing = TRUE)
 	return(list(newBest, scores))
@@ -48,16 +27,16 @@ generation <- function(curBest, mutator, scorer, population){
 
 weasel <- function(nGen=100, target="METHINKS IT IS LIKE A WEASEL", scorer=score, mutator=mutate){
 	str11 <- stri_rand_strings(1, nchar(target), pattern = "[A-Z]")
-	cBest <- generation(str11, mutate(), score(), 100)[1]
-	scoreHistory <- matrix(unlist(generation(str11, mutate(), score(), 100)[2]))
+	cBest <- generation(str11, mutate(), score(), 100, target)[1]
+	scoreHistory <- matrix(unlist(generation(str11, mutate(), score(), 100, target)[2]))
 	for (i in seq(nGen)){
-		lsBest <- generation(cBest, mutate(), score(), 100)
+		lsBest <- generation(cBest, mutate(), score(), 100, target)
 		cBest <- lsBest[1]
-		message("Maximum score of ", i, " generation: ", max(unlist(lsBest[2])))
-		message("Average score of ", i, " generation: ", mean(unlist(lsBest[2])), "\n")
 		message("String: ", cBest)
-		message(sprintf("Average (sprintf) score of %i generation: %f ", i, mean(unlist(lsBest[2]))))
-		scoreHistory <- cbind(scoreHistory, unlist(lsBest[2])) 
+		message("Maximum score of ", i, " generation: ", max(unlist(lsBest[2])))
+		message("Average score of ", i, " generation: ", mean(unlist(lsBest[2])))
+		message(sprintf("Average (sprintf) score of %i generation: %f \n", i, mean(unlist(lsBest[2]))))
+		scoreHistory <- cbind(scoreHistory, unlist(lsBest[2]))
 	}
 
 	return(list(cBest, scoreHistory))
